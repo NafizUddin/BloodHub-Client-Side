e36e2d974c673bc0e65609c65e83d37664ed1f72
NafizUddin <nafizuddin.ctg@gmail.com>
2023-11-24 17:25:47 +0600

    fixed blood sign in blood table

--- src/Home Components/Banner/Blood Table/BloodTable.jsx
+++ src/Home Components/Banner/Blood Table/BloodTable.jsx
@@ -13,7 +13,9 @@ const BloodTable = () => {
             </tr>
             <tr className="text-center">
               <th></th>
-              <th className="py-5 w-[10%] border-x-2 text-lg">Blood Type</th>
+              <th className="py-5 w-[10%] border-x-2 text-lg px-1">
+                Blood Type
+              </th>
               <th className="py-5 border-r-2 text-lg">O-</th>
               <th className="py-5 border-r-2 text-lg">O+</th>
               <th className="py-5 border-r-2 text-lg">B-</th>
@@ -29,15 +31,44 @@ const BloodTable = () => {
               <th
                 scope="row"
                 rowSpan={9}
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap -rotate-90 text-lg"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap -rotate-90 text-3xl w-[7%]"
               >
                 Recipient
               </th>
-              <td className="px-6 py-4 border-2 text-center">AB+</td>
-              <td className="px-6 py-4 text-center flex justify-center">
-                <FaDroplet className="text-[#D60C0C]" />
+              <td className="px-6 py-4 border-2 text-center text-lg text-black">
+                AB+
               </td>
-              <td className="px-6 py-4 text-center flex justify-center">Yes</td>
+              <td className="px-6 py-4 text-center text-xl border-t-2 border-r-2">
+                🩸
+              </td>
+              <td className="px-6 py-4 text-center text-xl border-t-2 border-r-2">
+                🩸
+              </td>
+              <td className="px-6 py-4 text-center text-xl border-t-2 border-r-2">
+                🩸
+              </td>
+              <td className="px-6 py-4 text-center text-xl border-t-2 border-r-2">
+                🩸
+              </td>
+              <td className="px-6 py-4 text-center text-xl border-t-2 border-r-2">
+                🩸
+              </td>
+              <td className="px-6 py-4 text-center text-xl border-t-2 border-r-2">
+                🩸
+              </td>
+              <td className="px-6 py-4 text-center text-xl border-t-2 border-r-2">
+                🩸
+              </td>
+              <td className="px-6 py-4 text-center text-xl border-t-2 border-r-2">
+                🩸
+              </td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <td className="px-6 py-4 border-2 text-center text-lg text-black">
+                AB-
+              </td>
+              <td className="px-6 py-4 text-center">🩸</td>
+              <td className="px-6 py-4 text-center">Yes</td>
               <td className="px-6 py-4 text-center">Yes</td>
               <td className="px-6 py-4 text-center">$2999</td>
               <td className="px-6 py-4 text-center">$2999</td>
@@ -45,93 +76,95 @@ const BloodTable = () => {
               <td className="px-6 py-4 text-center">$2999</td>
               <td className="px-6 py-4 text-center">$2999</td>
             </tr>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Microsoft Surface Pro
-              </th>
-              <td className="px-6 py-4">White</td>
-              <td className="px-6 py-4">Laptop PC</td>
-              <td className="px-6 py-4">$1999</td>
+            <tr className="bg-white dark:bg-gray-800">
+              <td className="px-6 py-4 border-2 text-center text-lg text-black">
+                A+
+              </td>
+              <td className="px-6 py-4 text-center flex justify-center">
+                <FaDroplet className="text-[#D60C0C]" />
+              </td>
+              <td className="px-6 py-4 text-center">Yes</td>
+              <td className="px-6 py-4 text-center">Yes</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
             </tr>
             <tr className="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
+              <td className="px-6 py-4 border-2 text-center text-lg text-black">
+                A-
+              </td>
+              <td className="px-6 py-4 text-center flex justify-center">
+                <FaDroplet className="text-[#D60C0C]" />
+              </td>
+              <td className="px-6 py-4 text-center">Yes</td>
+              <td className="px-6 py-4 text-center">Yes</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
             </tr>
             <tr className="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
+              <td className="px-6 py-4 border-2 text-center text-lg text-black">
+                B+
+              </td>
+              <td className="px-6 py-4 text-center flex justify-center">
+                <FaDroplet className="text-[#D60C0C]" />
+              </td>
+              <td className="px-6 py-4 text-center">Yes</td>
+              <td className="px-6 py-4 text-center">Yes</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
             </tr>
             <tr className="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
+              <td className="px-6 py-4 border-2 text-center text-lg text-black">
+                B-
+              </td>
+              <td className="px-6 py-4 text-center flex justify-center">
+                <FaDroplet className="text-[#D60C0C]" />
+              </td>
+              <td className="px-6 py-4 text-center">Yes</td>
+              <td className="px-6 py-4 text-center">Yes</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
             </tr>
             <tr className="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
+              <td className="px-6 py-4 border-2 text-center text-lg text-black">
+                O+
+              </td>
+              <td className="px-6 py-4 text-center flex justify-center">
+                <FaDroplet className="text-[#D60C0C]" />
+              </td>
+              <td className="px-6 py-4 text-center">Yes</td>
+              <td className="px-6 py-4 text-center">Yes</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
             </tr>
             <tr className="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
-            </tr>
-            <tr className="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
-            </tr>
-            <tr className="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td className="px-6 py-4">Black</td>
-              <td className="px-6 py-4">Accessories</td>
-              <td className="px-6 py-4">$99</td>
+              <td className="px-6 py-4 border-2 text-center text-lg text-black">
+                O-
+              </td>
+              <td className="px-6 py-4 text-center flex justify-center">
+                <FaDroplet className="text-[#D60C0C]" />
+              </td>
+              <td className="px-6 py-4 text-center">Yes</td>
+              <td className="px-6 py-4 text-center">Yes</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
+              <td className="px-6 py-4 text-center">$2999</td>
             </tr>
           </tbody>
         </table>
